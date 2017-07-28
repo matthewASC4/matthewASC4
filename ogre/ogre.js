8c12bb96ef7257582e1313028a90fd9830591d41
matthewASC4 <yankeematt123@gmail.com>
2017-07-28 12:43:23 -0700

    my website

---
+++ ogre/ogre.js
@@ -0,0 +1,13 @@
+var HP = 100;
+var gold = 0;
+var numberDefeated = 0;
+
+function updatedStats(){
+    $("#stats").tex("HP:" +)
+}
+function setup(){
+$("body").append(
+    "<h1> Welcome to Ogre Fighter v.1.0.</h1>"
+);
+}
+)
